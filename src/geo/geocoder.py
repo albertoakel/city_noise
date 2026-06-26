d5a3efdc6b8bbf0581c247769724b2e2f476f223
albertoakel <alberto.akel@gmail.com>
2026-06-26 14:10:54 -0300

    updates geocode update

--- src/geo/geocoder.py
+++ src/geo/geocoder.py
@@ -2,20 +2,20 @@
 from geopy.geocoders import Nominatim
 import time
 
-#geolocator = Nominatim(user_agent="city_noise_app")
+geolocator = Nominatim(user_agent="city_noise_app")
 
-geolocator = Nominatim(
-    user_agent="city_noise_app",
-    timeout=5
-)
+# geolocator = Nominatim(
+#     user_agent="city_noise_app",
+#     timeout=5
+# )
 
 def geocode_address(address: str):
     """Converte endereço em coordenadas"""
     try:
-        query = f"{address}, Belém, Pará, Brasil"
-        time.sleep(1)
-        location = geolocator.geocode(query)
-        print("DEBUG LOCATION:", location)
+        #query = f"{address}, Belém, Pará, Brasil"
+        #time.sleep(1)
+        location = geolocator.geocode()
+        #print("DEBUG LOCATION:", location)
         if location:
             return {
                 "lat": location.latitude,
@@ -32,7 +32,7 @@ def reverse_geocode(lat: float, lon: float):
     """Converte coordenadas em endereço"""
     try:
         location = geolocator.reverse((lat, lon), exactly_one=True)
-        time.sleep(1)  # respeitar limite do Nominatim
+        #time.sleep(1)  # respeitar limite do Nominatim
         if location:
             return location.address
         return None
